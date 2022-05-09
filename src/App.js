b607bfe7c370baf18857ccaf88cab8350e1f08e4
YildizAydogan <yildizaydoganjobmail@gmail.com>
2022-05-09 20:24:08 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Anasayfa from "./components/Anasayfa";
+import Footer from "./components/Footer";
+import Hakkimizda from "./components/Hakkimizda";
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+
+
+
+
+function App()
+ {
+  return(
+  <BrowserRouter>
+  <Header/>
+  <Footer/>
+
+  <Routes>
+ <Route path="/hakkimizda" element={<Hakkimizda/>}/>
+ <Route path="/login" element={<Login/>}/>
+ <Route path="/" element={<Anasayfa/>}/>
+  </Routes>
+
+
+
+
+  </BrowserRouter>
+  )
+  
+  }
+
+export default App;
